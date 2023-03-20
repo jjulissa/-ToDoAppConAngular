b7a2b3ebca953beb7ac644a56914c2cef189ddc9
jjulissa <julissajoseph9@gmail.com>
2023-03-20 16:51:03 +0100

    ToDoAppCon Angular & Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,54 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+}) 
+
 export class AppComponent {
-  title = 'my-first-project';
+  title: string = 'To Do App con Angular & Bootstrap';
+
+  employees = 
+  [ 
+    { 
+      name: "JJulissa", 
+      position: 'Developer', 
+      email: 'julissajoseph9@gmail.com'
+    }, 
+    { 
+      name: "Edkarlie JOSEPH", 
+      position: 'Enfermera', 
+      email: 'karlie@gmail.com'
+    }, 
+    { 
+      "name": "Jeff-Hermane Francois", 
+      position: 'Business Manager', 
+      email:'jeff@yahwoo.us'
+    }
+  ]; 
+
+  model:any = {}; 
+  model2:any = {};
+
+// TipoVoid: KiPapRetounenAnyen
+  addEmployee(): void { 
+    this.employees.push(this.model)
+
+  } 
+
+  deleteEmployee(i:any): void { 
+    
+  } 
+ 
+  myValue:any; 
+
+  editEmployee(i:any): void { 
+    this.model2.name = this.employees[i].name; 
+    this.model2.position = this.employees[i].position;  
+    this.model2.email = this.employees[i].email; 
+    this.myValue = i; 
+  } 
+
+  upDateEmployee(): void { 
+    console.log(this.model2);
+    
+  }
 }
